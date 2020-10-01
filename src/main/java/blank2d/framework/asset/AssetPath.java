4a1db9b6cc1388cb7f5c86bd25890c3eee14c6bd
jamescopping <james.copping97@gmail.com>
2020-10-01 22:52:49 +0100

    fixed #1 add some new exceptions to help with debugging

--- src/main/java/blank2d/framework/asset/AssetPath.java
+++ src/main/java/blank2d/framework/asset/AssetPath.java
@@ -1,5 +1,7 @@
 package blank2d.framework.asset;
 
+import blank2d.framework.ecs.system.PhysicsSystem;
+
 import java.net.URL;
 
 public final class AssetPath {
@@ -9,7 +11,16 @@ public final class AssetPath {
     private static String audioDirectory = "audio";
 
     public static URL getURL(String directory, String fileName){
-        String path = getResourceDirectory() + directory + "/" + fileName;
+        String path = "";
+        try {
+            if(directory.isEmpty()) throw new Exception("directory can't be empty, make sure you change it to the name of the directory with set###Directory()");
+            if(fileName.isEmpty()) throw new Exception("fileName given is empty");
+            if(!fileName.contains(".")) throw new Exception("fileName should contain a valid file extension e.g (.png or .mp3)");
+            path = getResourceDirectory() + directory + "/" + fileName;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return AssetPath.class.getResource(path);
     }
 
@@ -18,6 +29,7 @@ public final class AssetPath {
     }
 
     public static void setResourceDirectory(String resourceDirectory) {
+        if(!resourceDirectory.endsWith("/")) resourceDirectory = resourceDirectory + "/";
         AssetPath.resourceDirectory = resourceDirectory;
     }
 
@@ -26,6 +38,7 @@ public final class AssetPath {
     }
 
     public static void setSpriteDirectory(String spriteDirectory) {
+        assert !spriteDirectory.equals("");
         AssetPath.spriteDirectory = spriteDirectory;
     }
 
